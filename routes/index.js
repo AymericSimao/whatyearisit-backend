1aab94909e663a262b0c701992627bfb75a3c485
Aymeric <aymeric.simao@gmail.com>
2025-04-11 10:53:17 +0200

    first commit backend whatyearisit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/year', (req, res) => {
+  const date = new Date()
+  const year = date.getFullYear()
+  res.json({date : year})
+})
+
+module.exports = router;
